468fa4ccf7bd54cfead61d5383d751d4b1d1ceca
Gureet Sidhu <42849884+gs109111@users.noreply.github.com>
2025-12-07 11:09:52 -0700

    Proof Of Concept
    Run this script using python: main.py [zip_file]

---
+++ main.py
@@ -0,0 +1,22 @@
+# Modules
+import os
+import sys
+
+# Inital Variables / Input
+print("")
+
+zip_file = sys.argv[1]
+zip_file_size = int(os.popen(f"unzip -l {zip_file} | wc -l").read().strip().replace("\n", ""))
+os.system("clear")
+
+print("")
+
+print(f"Cracking Structure...")
+
+def flood(line):
+    os.system(f"unzip -o -P {line} {zip_file} &>/dev/null")
+
+for num in range(1, zip_file_size ** 2):
+    flood(num)
+
+print("")
